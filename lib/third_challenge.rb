69e42e8e0956507ce17af475b5cdecf88d4984eb
Ting Ting Ye <tingtingye24@gmail.com>
2019-08-13 20:54:54 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -29,7 +29,8 @@ def third_challenge
         #your key/value pairs here
         name: "Juliet", age: "15", status: "alive"
         },
-      :heroine_friends => []
+      :heroine_friends => [{name: "Steven", age: "30", attitude: "confused"},
+          {name: "Nurse", age: "44", attitude: "worried"}]
    }
   }
 
