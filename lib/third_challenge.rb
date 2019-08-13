b77778746886b2e8515a1b560992b7fdd9b84320
Ting Ting Ye <tingtingye24@gmail.com>
2019-08-13 20:54:22 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -13,7 +13,8 @@ def third_challenge
         #your key/value pairs here
         name: "Romeo", age: "15", status:'alive'
         },
-      :hero_friends => []
+      :hero_friends => [{name: "Benvolio", age: "17", attitude: "worried"},
+         {name: "Mercutio", age: "18", attitude: "hot-headed"}]
    }, 
    :capulet => {
       :patriarch => {
